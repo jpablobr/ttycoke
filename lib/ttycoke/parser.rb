8326b327ca53e6c8b8ad544f06c829031d52f69a
jpablobr <xjpablobrx@gmail.com>
2012-01-31 09:22:16 +1000

    fix: Parser should ignore unmatched lines
    When TTYCoke::Parser parses a program which already exist in the
    config file it should ignore the lines that don't match the regex.

--- lib/ttycoke/parser.rb
+++ lib/ttycoke/parser.rb
@@ -6,11 +6,15 @@ module TTYCoke
     def self.coke! prgm, line
       mtchd, coked_str = prgm.fetch('regex').match(line), ''
       log_rescue(self, __method__, caller, {prgm: prgm, line: line}) {
-        (mtchd.size - 1).times do |i|
-          coked_str << mtchd[i+1].send(prgm.fetch('clrs').fetch(i))
-        end unless !mtchd
-        coked_str
-       }
+        if mtchd
+          (mtchd.size - 1).times do |i|
+            coked_str << mtchd[i+1].send(prgm.fetch('clrs').fetch(i))
+          end
+          coked_str
+        else
+          line
+        end
+      }
     end
   end
 end
